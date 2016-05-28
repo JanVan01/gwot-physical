c7b9ee3413b58015981582bb0d760137ff47bf84
Trzaska <n_trza01@uni-muenster.de>
2016-05-28 12:45:20 +0200

    Try notification with method everyMinute. Idea: Use cron to call the method. Method reads data from DB.

--- publisher.py
+++ publisher.py
@@ -1,17 +1,22 @@
 import paho.mqtt.publish as publish
-#publish.single("paho/test/single", "boo", hostname="test.mosquitto.org")
-publish.single("everyMeasurement","everyMeasurementMessage", hostname="localhost")
 
-publish.single("every5Minutes","every5MinutesMessage", hostname="localhost")
+class Publisher:
 
-publish.single("every10Minutes","every10MinutesMessage", hostname="localhost")
+    def everyMinute():
+        data = data = db.get_last_measurement(get_filter())
+        publish.single("everyMinute", "send_json(data)", hostname="localhost")
 
-publish.single("every15Minutes","every15MinutesMessage", hostname="localhost")
 
-publish.single("every30Minutes","every30MinutesMessage", hostname="localhost")
-
-publish.single("every45Minutes","every45MinutesMessage", hostname="localhost")
-
-publish.single("daylyReport","daylyReport", hostname="localhost")
-
-publish.single("daylyReport","daylyReport", hostname="localhost")
+# publish.single("everyMeasurement","everyMeasurementMessage", hostname="localhost")
+#
+# publish.single("every5Minutes","every5MinutesMessage", hostname="localhost")
+#
+# publish.single("every10Minutes","every10MinutesMessage", hostname="localhost")
+#
+# publish.single("every15Minutes","every15MinutesMessage", hostname="localhost")
+#
+# publish.single("every30Minutes","every30MinutesMessage", hostname="localhost")
+#
+# publish.single("every45Minutes","every45MinutesMessage", hostname="localhost")
+#
+# publish.single("daylyReport","daylyReport", hostname="localhost")
