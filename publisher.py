eeef64288e4a03e106f10228c3627377e8106883
Trzaska <n_trza01@uni-muenster.de>
2016-05-28 14:26:16 +0200

    Try to solve it dirty to work on

--- publisher.py
+++ publisher.py
@@ -1,12 +1,51 @@
 import paho.mqtt.publish as publish
-import server.get_filter as filter
 import json
 from datetime import datetime
 from database.database import Database
+from flask import Flask, Response, request
+
+
+def get_filter():
+    # ToDo: Add proper variable checks / sanitation
+    outliers = request.args.get('outliers')
+    start = request.args.get('start')
+    end = request.args.get('end')
+    location = request.args.get('location')
+    coordinates = request.args.get('coordinates')
+
+    args = {
+        'outliers': None,
+        'start': None,
+        'end': None,
+        'location': None,
+        'coordinates': None
+    }
+
+    if outliers != None and len(outliers) > 0 and (outliers == 0 or outliers == 1):
+        args['outliers'] = outliers
+
+    # ToDo
+    if start != None and len(start) > 0:
+        args['start'] = start
+
+    # ToDo
+    if end != None and len(end) > 0:
+        args['end'] = end
+
+    if location != None and location > 0:
+        args['location'] = location
+
+    # ToDo
+    if coordinates != None and len(coordinates) > 0:
+        args['coordinates'] = coordinates
+
+    return args
+
+
 
 db = Database()
 
-data = db.get_last_measurement(filter)
+data = db.get_last_measurement(serv.get_filter())
 publish.single("everyMinute", "send_json(data)", hostname="localhost")
 
 
