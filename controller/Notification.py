0c3c618ebd0d4c0f7dec9e5567c556201f306e66
Trzaska <n_trza01@uni-muenster.de>
2016-06-12 14:41:06 +0200

    Syntax error

--- controller/Notification.py
+++ controller/Notification.py
@@ -13,7 +13,7 @@ def publishEveryMeasurement(data):
     message = str(data[0].value).encode('unicode_escape')
     publish.single("everyMeasurement", message, hostname="localhost")
 
-def publishByThreshold((data):
+def publishByThreshold(data):
     if(data[0].value > 200)
         message = str(data[0].value).encode('unicode_escape')
         publish.single("publishByThreshold", message, hostname="localhost")
