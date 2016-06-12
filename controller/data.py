01c463336f3de782ed5c2e570e22f8f94acf8295
Trzaska <n_trza01@uni-muenster.de>
2016-06-12 11:03:13 +0200

    updated importstatement

--- controller/data.py
+++ controller/data.py
@@ -1,6 +1,6 @@
 from flask import request
 from controller.base import BaseController
-from controler.Notification import publishEveryMeasurement
+from controller.Notification import publishEveryMeasurement
 
 class DataController(BaseController):
 
