e1d91056281aec309c9b6015eed02b9c3fd7f01d
Trzaska <n_trza01@uni-muenster.de>
2016-06-12 14:17:37 +0200

    .value was at a so wrong place

--- controller/Notification.py
+++ controller/Notification.py
@@ -3,5 +3,5 @@ import paho.mqtt.publish as publish
     # Takes some input (data) and pushs it to the MQTT-broker 'mosquitto'.
     # Subscrubers of the channel 'everyMeasurement' will recieve
 def publishEveryMeasurement(data):
-    message = str(data).encode('unicode_escape')
-    publish.single("everyMeasurement", message.value, hostname="localhost")
+    message = str(data.value).encode('unicode_escape')
+    publish.single("everyMeasurement", message, hostname="localhost")
