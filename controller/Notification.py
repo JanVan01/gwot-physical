5190d7ae67a2e111c5803cbef403f170335cbe90
Trzaska <n_trza01@uni-muenster.de>
2016-06-12 14:20:20 +0200

    Ahhh working with a list...

--- controller/Notification.py
+++ controller/Notification.py
@@ -3,5 +3,5 @@ import paho.mqtt.publish as publish
     # Takes some input (data) and pushs it to the MQTT-broker 'mosquitto'.
     # Subscrubers of the channel 'everyMeasurement' will recieve
 def publishEveryMeasurement(data):
-    message = str(data.value).encode('unicode_escape')
+    message = str(data[0].value).encode('unicode_escape')
     publish.single("everyMeasurement", message, hostname="localhost")
