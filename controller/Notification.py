2f4912826d88a97ac1556dcf5b2ab38260895459
Trzaska <n_trza01@uni-muenster.de>
2016-06-12 11:08:37 +0200

    removed syntaxerror

--- controller/Notification.py
+++ controller/Notification.py
@@ -5,6 +5,6 @@ class Notification(BaseController):
 
     # Takes some input (data) and pushs it to the MQTT-broker 'mosquitto'.
     # Subscrubers of the channel 'everyMeasurement' will recieve the data.
-    def publishEveryMeasurement(self, data)
+    def publishEveryMeasurement(self, data):
         message = str(data).encode('unicode_escape')
         publish.single("everyMeasurement", message, hostname="localhost")
