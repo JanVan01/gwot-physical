ab948435c18ac371fe153225cc317a9d17095e83
Trzaska <n_trza01@uni-muenster.de>
2016-06-12 14:30:16 +0200

    Added further channel

--- controller/Notification.py
+++ controller/Notification.py
@@ -2,6 +2,16 @@ import paho.mqtt.publish as publish
 
     # Takes some input (data) and pushs it to the MQTT-broker 'mosquitto'.
     # Subscrubers of the channel 'everyMeasurement' will recieve
+
+def publish Measurement(data):
+    publishEveryMeasurement(data)
+    publishByThreshold(data)
+
 def publishEveryMeasurement(data):
     message = str(data[0].value).encode('unicode_escape')
     publish.single("everyMeasurement", message, hostname="localhost")
+
+def publishByThreshold((data):
+    if(data[0].value > 200)
+        message = str(data[0].value).encode('unicode_escape')
+        publish.single("publishByThreshold", message, hostname="localhost")
