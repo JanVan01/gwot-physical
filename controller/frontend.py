c400ef5754cda7199fed41757e8ade7aadd15b08
JanVan01 <j_vanz01@uni-muenster.de>
2016-07-06 23:46:36 +0200

    create templates for frontend

--- controller/frontend.py
+++ controller/frontend.py
@@ -1,37 +1,41 @@
 from controller.base import BaseController
+from views.html import HtmlView
 
 class FrontendController(BaseController):
 
 	def __init__(self):
 		super().__init__()
-		self.multi_model = self.get_model('models.measurements', 'Measurements')
 
-	def get_view(self, template_file = None):
+	def get_view(self, template_file=None):
 		view = HtmlView()
 		if template_file is not None:
 			view.set_template(template_file)
 		return view
 
 	def home(self):
-		data = self.multi_model.get_all()
-		return self.get_view().data(data)
+		data = {}
+		return self.get_view('index.html').data(data)
 
 	def config(self):
-		data = self.multi_model.get_all()
-		return self.get_view().data(data)
+		data = {}
+		return self.get_view('config.html').data(data)
 
 	def config_password(self):
-		data = self.multi_model.get_all()
-		return self.get_view().data(data)
+		data = {}
+		return self.get_view('config_password.html').data(data)
 
 	def config_sensors(self):
-		data = self.multi_model.get_all()
-		return self.get_view().data(data)
+		data = {}
+		return self.get_view('config_sensors.html').data(data)
 
 	def config_locations(self):
-		data = self.multi_model.get_all()
-		return self.get_view().data(data)
+		data = {}
+		return self.get_view('config_locations.html').data(data)
 
 	def data(self):
-		data = self.multi_model.get_all()
-		return self.get_view().data(data)
+		data = {}
+		return self.get_view('data.html').data(data)
+
+	def about(self):
+		data = {}
+		return self.get_view('about.html').data(data)
