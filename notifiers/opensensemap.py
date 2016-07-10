2eec666d2660f5870907c73f11610acda3c38f67
Matthias Mohr <webmaster@mamo-net.de>
2016-07-10 17:00:12 +0200

    IMplemented OpenSenseMap notificatiion

--- notifiers/opensensemap.py
+++ notifiers/opensensemap.py
@@ -1,6 +1,16 @@
 from notifiers.base import BaseNotifier
+import requests
 
 class OpenSenseMapNotifier(BaseNotifier):
 
 	def send(self, notifier, subscriber, measurement):
-		print("Sending OpenSenseMap notification to " + subscriber.get_connector() + " with value " + str(measurement.get_value()))
+		sensebox_id = "" # ToDo: Get from settings
+		sensor_id = "" # ToDo: Get from settings
+
+		data = {"value": measurement.get_value()}
+		url = "http://www.opensensemap.org:8000/boxes/" + sensebox_id + "/" + sensor_id
+		headers = {"Content-type": "application/json", "Connection": "close"}
+
+		r = requests.post(url, headers=headers, json=data)
+
+		print("Sending OpenSenseMap notification to " + subscriber.get_connector() + " with value " + str(measurement.get_value()) + " and got response " + str(r.status_code))
