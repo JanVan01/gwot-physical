b4e1b8ca624856fcf05b286f3adb10b13822dedd
Matthias Mohr <webmaster@mamo-net.de>
2016-07-18 03:51:37 +0200

    Basics for Admin CP, implemented password change.

---
+++ static/admin.js
@@ -0,0 +1,80 @@
+function error_msg(message) {
+	alert_msg(message, 'danger');
+}
+
+function warning_msg(message) {
+	alert_msg(message, 'warning');
+}
+
+function info_msg(message) {
+	alert_msg(message, 'info');
+}
+
+function success_msg(message) {
+	alert_msg(message, 'success');
+}
+
+function dismiss_alert() {
+	$('#alert').hide();
+}
+
+function alert_msg(message, classy) {
+	$('#alert').removeClass('alert-danger');
+	$('#alert').removeClass('alert-warning');
+	$('#alert').removeClass('alert-info');
+	$('#alert').removeClass('alert-success');
+	$('#alert').addClass("alert-"+classy);
+	$('#alert_message').html(message);
+	$('#alert').show();
+}
+
+function request(method, url, success, data) {
+	var options = {
+		url: url,
+		method: method,
+		error: function (ajax, status, error) {
+			error_msg('<strong>Error!</strong> Sorry, the requested action could not be executed.');
+		},
+		contentType: 'application/json',
+		dataType: "json",
+		headers: {
+			accept: 'application/json'
+		}
+	};
+	if (typeof success === 'function') {
+		options.success = function (data, status, ajax) {
+			success(data);
+			$('#ajax_form').reset();
+		};
+	}
+	else if (typeof success === 'string') {
+		options.success = function (data, status, ajax) {
+			success_msg(success);
+			$('#ajax_form').reset();
+		};
+	}
+	if (data) {
+		options.processData = false;
+		options.data = JSON.stringify(data);
+		console.log(options.data);
+	}
+	$.ajax(options);
+}
+
+function change_password() {
+	pw1 = $('#password').val();
+	pw2 = $('#password2').val();
+	if (pw1.length < 4) {
+		error_msg('<strong>Error!</strong> Please specify a password with at least 4 chars..');
+		return;
+	}
+	if (pw1 != pw2) {
+		error_msg("<strong>Error!</strong> The passwords don't match.");
+		return;
+	}
+	request(
+		'PUT', '/api/1.0/config/password',
+		'The password has been changed successfully and will be needed for the next login.',
+		{password: pw1}
+	);
+}
