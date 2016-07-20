d90d8dc94cd726452b5233d30d4c9b8810ea61ce
JanVan01 <j_vanz01@uni-muenster.de>
2016-07-20 10:47:13 +0200

    allow the configuration of messages published by mqtt

--- notifiers/mqtt.py
+++ notifiers/mqtt.py
@@ -1,33 +1,23 @@
 from notifiers.base import BaseNotifier
 # needed to push messages to MQTT
 import paho.mqtt.publish as publish
-from utils.utils import SettingManager
 import re
+from utils.utils import SettingManager
 
 # Takes some input (data) and pushs it to the MQTT-broker 'mosquitto'.
 # Subscrubers of the channel 'everyMeasurement' will recieve all measurements
+
 
 class MqttNotifier(BaseNotifier):
 
 	def send(self, notifier, subscriber, measurement):
 		value = measurement.get_value()
 		threshold = subscriber.get_setting('threshold')
-		if value > threshold:
-			topic = subscriber.get_setting('topic')
+		topic = subscriber.get_setting('topic')
+		if threshold == '':
 			publish.single(topic, str(value), hostname="localhost")
-
-		#self._publish_every_measurement(notifier, subscriber, measurement)
-		#self._publish_by_threshold(notifier, subscriber, measurement)
-
-	def _publish_every_measurement(self, notifier, subscriber, measurement):
-		message = str(measurement.get_value()).encode('unicode_escape')
-		publish.single("everyMeasurement", message, hostname="localhost")
-
-	def _publish_by_threshold(self, notifier, subscriber, measurement):
-		value = measurement.get_value()
-		if value > 200:
-			message = str(value).encode('unicode_escape')
-			publish.single("publishByThreshold", message, hostname="localhost")
+		elif value > float(threshold):
+			publish.single(topic, str(value), hostname="localhost")
 
 	def get_subscriber_settings(self):
 		return ['topic', 'threshold']
@@ -42,9 +32,11 @@ class MqttNotifier(BaseNotifier):
 
 	def validate_setting(self, key, value):
 		if key == "threshold":
+			if value == '':
+				return True
 			regexp = '^[-+]?\d+\.?\d*$'
 			return (re.match(regexp, value) is not None)
-		elif key == 'topic':
+		elif key == 'topic' and value != '':
 			return True
 		else:
 			return False
@@ -54,3 +46,6 @@ class MqttNotifier(BaseNotifier):
 			return SettingManager().get_input_field(key, value)
 		else:
 			return None
+
+	def is_public(self):
+		return False
